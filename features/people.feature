aedd093b80229c46389e71d992f048cb8d1e4bb4
Abdias Alpire <abdiasalpire12@gmail.com>
2024-04-07 21:36:32 -0400

    Scenario of fthe creation o a new person

---
+++ features/people.feature
@@ -0,0 +1,6 @@
+# Created by abdias.alpire at 7/4/24
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    # Enter steps here
--- utilities/a.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-from selenium.webdriver.chrome import webdriver
-
-from selenium.webdriver import Chrome
-
-driver = Chrome()
-driver.maximize_window()
-driver.get('https://cloud.idurarapp.com/login')
